3573ea2be3d44e856980ccff3f58c00eda8c89f8
priyanshnihalani <priyansh.nihalani@gmail.com>
2024-11-12 12:46:27 +0530

    Gold-Loan-Admin

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig1 = {
-  apiKey: "AIzaSyBloTqs7q5RjzATueQnn22OvNgdm7BVV4o",
-  authDomain: "gold-loan-admin.firebaseapp.com",
-  projectId: "gold-loan-admin",
-  storageBucket: "gold-loan-admin.appspot.com",
-  messagingSenderId: "450868328140",
-  appId: "1:450868328140:web:1dfa40099c23cb508db469"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY1,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN1,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID1,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET1,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID1,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID1,
 };
 
 const firebaseConfig2 = {
-  apiKey: "AIzaSyAR2mw6erM0zEJp6xEGRn6Q_KrTriEYix4",
-  authDomain: "goldloan-dbe36.firebaseapp.com",
-  projectId: "goldloan-dbe36",
-  storageBucket: "goldloan-dbe36.appspot.com",
-  messagingSenderId: "242296268770",
-  appId: "1:242296268770:web:49b0a56cc9697bdf6a7bbb"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app1 = initializeApp(firebaseConfig1, 'app1');
